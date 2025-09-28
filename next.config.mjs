874d6d64fa8fa86d3b533d31fa80e17857c3ab10
Yukai Huang <yukaihuangtw@gmail.com>
2025-09-28 16:49:40 +0800

    chore: configured sample audio files caching

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,36 @@ const withPWA = withPWAInit({
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   buildExcludes: [/middleware-manifest\.json$/],
+  runtimeCaching: [
+    {
+      urlPattern: ({ url }) => url.pathname.startsWith('/audio/'),
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'henka-audio-samples',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [200],
+        },
+      },
+    },
+    {
+      urlPattern: ({ request }) => request.destination === 'audio',
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'henka-audio-streams',
+        expiration: {
+          maxEntries: 40,
+          maxAgeSeconds: 60 * 60 * 24 * 14, // 14 days
+        },
+        cacheableResponse: {
+          statuses: [200],
+        },
+      },
+    },
+  ],
 })
 
 /** @type {import('next').NextConfig} */
